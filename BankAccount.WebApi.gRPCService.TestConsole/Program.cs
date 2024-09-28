f3668b13427997ed464c9cb12e2048d425f81d81
Jose Gomes <jose.fernando.ferreira.gomes@outlook.com>
2024-09-28 12:56:37 +0100

    Greeting service works, tested by console

---
+++ BankAccount.WebApi.gRPCService.TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BankAccount.WebApi.gRPCService;
+using Grpc.Net.Client;
+using System.Threading.Tasks;
+using BankAccount.GrpcGreeterClient;
+
+Console.WriteLine("Hello, World! ... waiting for service to start...");
+
+Task.Delay(5000).Wait();
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:32769");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+
+var bankingClient = new BankService.BankServiceClient(channel);
+var bankingReply = bankingClient.GetCustomer(new CustomerRequest() { CustomerId = 1} );
+                  
+Console.WriteLine("banking customer: " + bankingReply.FirstName);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
--- BankAccount.WebApi.gRPCService/Services/BankServiceImpl.cs
+++ BankAccount.WebApi.gRPCService/Services/BankServiceImpl.cs
@@ -1,56 +1,57 @@
-﻿//using System.Threading.Tasks;
-//using Grpc.Core;
-//using BankAccount.Grpc;  // Namespace generated from your .proto file
-////using BankAccount.WebAPI.DAL;
-//using BankAccount.WebApi.BL;
-//using global::Grpc.Core;
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using BankAccount.Grpc;  // Namespace generated from your .proto file
+//using BankAccount.WebAPI.DAL;
+using BankAccount.WebApi.BL;
+using global::Grpc.Core;
 
-//namespace BankAccount.WebApi.gRPCService.Services
-//{    
+namespace BankAccount.WebApi.gRPCService.Services
+{
 
-//    public class BankServiceImpl : BankService.BankServiceBase
-//    {
-//        private readonly CustomerService _customerService;
+    public class BankServiceImpl : BankService.BankServiceBase
+    {
+        private readonly CustomerService _customerService;
 
-//        public BankServiceImpl(CustomerService customerService)
-//        {
-//            _customerService = customerService;
-//        }
+        public BankServiceImpl(CustomerService customerService)
+        {
+            _customerService = customerService;
+        }
 
-//        // Implement the GetCustomer RPC
-//        public override async Task<CustomerResponse> GetCustomer(CustomerRequest request, ServerCallContext context)
-//        {
-//            var customer = await _customerService.GetCustomerByIdAsync(request.CustomerId);
+        // Implement the GetCustomer RPC
+        public override async Task<CustomerResponse> GetCustomer(CustomerRequest request, ServerCallContext context)
+        {
+            var customer = await _customerService.GetCustomerByIdAsync(request.CustomerId);
 
-//            return new CustomerResponse
-//            {
-//                CustomerId = customer.CustomerId,
-//                FirstName = customer.FirstName,
-//                LastName = customer.LastName,
-//                Email = customer.Email,
-//                PhoneNumber = customer.PhoneNumber
-//            };
-//        }
+            return new CustomerResponse
+            {
+                CustomerId = customer.CustomerId,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Email = customer.Email,
+                PhoneNumber = customer.PhoneNumber
+            };
+        }
 
-//        // Implement the CreateCustomer RPC
-//        public override async Task<CustomerResponse> CreateCustomer(CreateCustomerRequest request, ServerCallContext context)
-//        {
-//            var customer = await _customerService.CreateCustomerAsync(
-//                request.FirstName,
-//                request.LastName,
-//                request.Email,
-//                request.PhoneNumber
-//            );
+        // Implement the CreateCustomer RPC
+        public override async Task<CustomerResponse> CreateCustomer(CreateCustomerRequest request, ServerCallContext context)
+        {
+            var customer = await _customerService.CreateCustomerAsync(
+                request.FirstName,
+                request.LastName,
+                request.Email,
+                request.PhoneNumber,
+                DateTime.Parse(request.DateOfBirth)
+            );
 
-//            return new CustomerResponse
-//            {
-//                CustomerId = customer.CustomerId,
-//                FirstName = customer.FirstName,
-//                LastName = customer.LastName,
-//                Email = customer.Email,
-//                PhoneNumber = customer.PhoneNumber
-//            };
-//        }
-//    }
+            return new CustomerResponse
+            {
+                CustomerId = customer.CustomerId,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Email = customer.Email,
+                PhoneNumber = customer.PhoneNumber
+            };
+        }
+    }
 
-//}
+}
